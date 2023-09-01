716074ea758c612ce2645ad46932e29567bc0e72
atula28os <atula28os@gmail.com>
2023-09-01 19:48:08 +0530

    added logic of Queue

--- workers/YahooFinanceWorkers.py
+++ workers/YahooFinanceWorkers.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable, Iterable, Mapping
 import threading
 from typing import Any
 import requests
@@ -7,7 +8,25 @@ import json
 logger = logging.getLogger()
 logger.setLevel(level=logging.INFO)
 
-class YahooFinancePriceWorker(threading.Thread):
+class YahooFinancePriceScheduler(threading.Thread):
+    def __init__(self, input_queue, **kwargs):
+        super(YahooFinancePriceScheduler,self).__init__(**kwargs)
+        self._input_queue = input_queue
+        self.start()
+        
+    def run(self):
+        while True:
+            val = self._input_queue.get()
+            if val == 'DONE':
+                break 
+            
+            yahoo_finance_price_worker = YahooFinancePriceWorker(symbol=val)
+            price = yahoo_finance_price_worker.get_price()
+            # self._output_queue.put(json.dumps({'symbol': val, 'price': price}))
+            print(price)
+
+
+class YahooFinancePriceWorker():
     def __init__(self, symbol, *args, **kwargs):
         super(YahooFinancePriceWorker,self).__init__(*args, **kwargs)
         self._symbol = symbol
@@ -15,7 +34,6 @@ class YahooFinancePriceWorker(threading.Thread):
         base_url = 'https://www.alphavantage.co/query'
         self._url = f'{base_url}?function=GLOBAL_QUOTE&symbol={self._symbol}&apikey={api_keys}'
         self._price = None
-        self.start()
         
     def get_price(self):        
         try:
@@ -23,12 +41,9 @@ class YahooFinancePriceWorker(threading.Thread):
             data = response.json()
             self._price = float(data['Global Quote']["05. price"])
             print(f"Price for {self._symbol}: {self._price}")
+            return self._price
         except Exception as ex:
             logger.warning(f"Not able to find the price for {self._symbol}")
-
-    def run(self):
-        self.get_price()
-    
     
     
         
